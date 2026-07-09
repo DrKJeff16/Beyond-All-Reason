b65a1797c02369d033758435dd6ac415fb822b8b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-07-09 00:32:48 -0600

    Update LuaFont.lua

--- types/LuaFont.lua
+++ types/LuaFont.lua
@@ -3,13 +3,55 @@
 --- Engine font object from `gl.LoadFont` (subset for LuaUI/LuaRules).
 
 ---@class LuaFont
----@field Begin fun(self: LuaFont, userDefinedBlending: boolean?)
----@field End fun(self: LuaFont)
----@field Print fun(self: LuaFont, text: string, x: number, y: number, size: number?, options: string?)
----@field PrintWorld fun(self: LuaFont, text: string, x: number, y: number, z: number, size: number?, options: string?)
----@field SetTextColor fun(self: LuaFont, color: table|number, g: number?, b: number?, a: number?)
----@field SetOutlineColor fun(self: LuaFont, color: table|number, g: number?, b: number?, a: number?)
----@field GetTextWidth fun(self: LuaFont, text: string): number
----@field GetTextHeight fun(self: LuaFont, text: string): number, number, number
----@field WrapText fun(self: LuaFont, text: string, maxWidth: number, maxHeight: number?, size: number?): string, number
 ---@field [string] any
+local LuaFont = {}
+
+---@param userDefinedBlending? boolean
+function LuaFont:Begin(userDefinedBlending) end
+
+function LuaFont:End() end
+
+---@param text string
+---@param x number
+---@param y number
+---@param size? number
+---@param options? string
+function LuaFont:Print(text, x, y, size, options) end
+
+---@param text string
+---@param x number
+---@param y number
+---@param z number
+---@param size? number
+---@param options? string
+function LuaFont:PrintWorld(text, x, y, z, size, options) end
+
+---@param color table|number
+---@param g? number
+---@param b? number
+---@param a? number
+function LuaFont:SetTextColor(color, g, b, a) end
+
+---@param color table|number
+---@param g? number
+---@param b? number
+---@param a? number
+function LuaFont:SetOutlineColor(color, g, b, a) end
+
+---@param text string
+---@return number
+function LuaFont:GetTextWidth(text) end
+
+---@param text string
+---@return number
+---@return number
+---@return number
+function LuaFont:GetTextHeight(text) end
+
+---@param text string
+---@param maxwidth number
+---@param maxHeight? number
+---@param size? number
+---@return string
+---@return number
+function LuaFont:WrapText(text, maxWidth, maxHeight, size) end
