135cead173cd726861c48609c39e1e0149a2cebc
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-07-09 00:13:48 -0600

    Update Callins.lua

--- types/Callins.lua
+++ types/Callins.lua
@@ -6,17 +6,42 @@
 -- Adding `...: any` makes these pass type checking.
 
 ---@class Callins
----@field UnitCreated fun(self, unitID: integer, unitDefID: integer, unitTeam: integer, builderID: integer?, ...: any)?
----@field UnitDestroyed fun(self, unitID: integer, unitDefID: integer, unitTeam: integer, attackerID: integer?, attackerDefID: integer?, attackerTeam: integer?, weaponDefID: integer?, ...: any)?
----@field UnitGiven fun(self, unitID: integer, unitDefID: integer, newTeam: integer, oldTeam: integer, ...: any)?
----@field UnitTaken fun(self, unitID: integer, unitDefID: integer, newTeam: integer, oldTeam: integer, ...: any)?
----@field GameFrame fun(self, frame: integer, ...: any)?
----@field PlayerChanged fun(self, playerID: integer, ...: any)?
----@field ViewResize fun(self, viewSizeX: integer, viewSizeY: integer, ...: any)?
----@field MouseMove fun(self, x: number, y: number, dx: number, dy: number, button: number, ...: any): boolean?
----@field MousePress fun(self, x: number, y: number, button: number, ...: any): boolean?
----@field MouseRelease fun(self, x: number, y: number, button: number, ...: any): boolean|integer
----@field TextInput fun(self, utf8: string, ...: any): boolean?
----@field UnitFinished fun(self, unitID: integer, unitDefID: integer, unitTeam: integer, ...: any)?
----@field FeatureCreated fun(self, featureID: integer, allyTeamID: integer, ...: any)?
----@field FeatureDestroyed fun(self, featureID: integer, allyTeamID: integer|boolean?, ...: any)?
+---@field UnitCreated? fun(self, unitID: integer, unitDefID: integer, unitTeam: integer, builderID: integer?, ...: any)
+---@field UnitDestroyed? fun(self, unitID: integer, unitDefID: integer, unitTeam: integer, attackerID: integer?, attackerDefID: integer?, attackerTeam: integer?, weaponDefID: integer?, ...: any)
+---@field UnitGiven? fun(self, unitID: integer, unitDefID: integer, newTeam: integer, oldTeam: integer, ...: any)
+---@field UnitTaken? fun(self, unitID: integer, unitDefID: integer, newTeam: integer, oldTeam: integer, ...: any)
+---@field GameFrame? fun(self, frame: integer, ...: any)
+---@field PlayerChanged? fun(self, playerID: integer, ...: any)
+---@field ViewResize? fun(self, viewSizeX: integer, viewSizeY: integer, ...: any)
+---@field UnitFinished? fun(self, unitID: integer, unitDefID: integer, unitTeam: integer, ...: any)
+---@field FeatureCreated? fun(self, featureID: integer, allyTeamID: integer, ...: any)
+---@field FeatureDestroyed? fun(self, featureID: integer, allyTeamID: integer|boolean?, ...: any)
+local Callins = {}
+
+---@param utf8 string
+---@param ... any
+---@return boolean|?
+function Callins:TextInput(utf8, ...) end
+
+---@param x number
+---@param y number
+---@param dx number
+---@param dy number
+---@param button number
+---@param ... any
+---@return boolean|?
+function Callins:MouseMove(x, y, dx, dy, button, ...) end
+
+---@param x number
+---@param y number
+---@param button number
+---@param ... any
+---@return boolean|?
+function Callins:MousePress(x, y, button, ...) end
+
+---@param x number
+---@param y number
+---@param button number
+---@param ... any
+---@return boolean|integer
+function Callins:MouseRelease(x, y, button, ...) end
